37adec6b623b789849758c3b1b6c987aa9344826
matiasborrazas <matias.borrazas@geopagos.com>
2022-06-09 05:25:17 -0300

    message input

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -4,7 +4,14 @@ import "./search.scss";
 function Search({submitForm, search, setSearch}){
     return (
         <form className="searchForm" onSubmit={submitForm}>
-            <input type="text"  className="searchInput" value={search} onChange={(e) => setSearch(e.target.value)} />
+            <input
+                type="text"  
+                className="searchInput" 
+                value={search} onChange={(e) => setSearch(e.target.value)}
+                aria-label="Ingresá lo que quieras encontrar"
+                placeholder="Nunca dejes de buscar"
+                maxLength="120" 
+            />
             <button type="submit" className="searchBtn" tabIndex="3" />
         </form>
     )
